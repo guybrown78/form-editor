f122ca0ccd08428a8a6b2ceabd96a080ccec9d9c
Guy Brown <guy@digitalopolis.co.uk>
2021-08-09 21:20:40 +0100

    tidy up

--- projects/tf-ng-form-editor/src/lib/tf-ng-form-editor.module.ts
+++ projects/tf-ng-form-editor/src/lib/tf-ng-form-editor.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+
 import { TfNgCoreModule } from 'tf-ng-core'
 import { TfNgFormModule} from 'tf-ng-form'
 
