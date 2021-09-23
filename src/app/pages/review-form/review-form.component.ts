b8766846fb12f31f74096f6a30c64d697bac7bdc
Guy Brown <guy@digitalopolis.co.uk>
2021-09-23 19:04:35 +0100

    example review page tidy up

--- src/app/pages/review-form/review-form.component.ts
+++ src/app/pages/review-form/review-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { TfNgFormService, TfNgFormPermissionService, DisplayJsonService } from 'tf-ng-form';
+import { TfNgFormService, TfNgFormPermissionService } from 'tf-ng-form';
 import { Subscription } from 'rxjs';
 
 
@@ -8,7 +8,7 @@ import { Subscription } from 'rxjs';
   templateUrl: './review-form.component.html',
   styleUrls: ['./review-form.component.css']
 })
-export class ReviewFormComponent implements OnInit {
+export class ReviewFormComponent implements OnInit, OnDestroy {
 
 
   submittedSubscription:Subscription;
@@ -16,23 +16,22 @@ export class ReviewFormComponent implements OnInit {
   constructor(
     private formService:TfNgFormService,
     private formPermissionService:TfNgFormPermissionService,
-    private displayJsonService:DisplayJsonService,
   ) { }
 
   ngOnInit(): void {
-    //
+
+		// Not sure why this is set in the class https://bitbucket.org/aisneutron/transform.formbuilder/src/TRAN-1341_August/Transform.FormBuilder.Client/src/app/admin/registrations/view-registration/view-registration.component.ts
+
+		// disableForm shouldn't be used in this scenario. formPermissionsService.formReadOnly is the param that should be set as this just stops all elements from being filled in, but still allows steppers/tabs/sections to be interacted with
+
+		// if you do need to set this, revert it back in the destory methos
+
+		// this.formService.disableForm = true;
 
     this.formPermissionService.formReadOnly = true;
     const url = 'assets/forms/review.json'
-    // const url = 'assets/forms/TraineeCADOperator_Americas.json'
     this.formService.getData(url).subscribe(data => {
       // data has loaded, the formService getData parses the data before it is returned here and stores the formFields, model and meta for the form.
-      // only thing left to do is listen for the submit button to be pressed
-      // this.submittedSubscription = this.formService.submitted.subscribe(
-      //   submittedJSON => {
-      //     this.onFormSubmitted(submittedJSON);
-      //   }
-      // )
     }, err => {
       // if the data can't load, then make the UI nice and show the user
       console.log("error loading data in controllong app")
@@ -40,18 +39,13 @@ export class ReviewFormComponent implements OnInit {
     })
   }
 
-  // onFormSubmitted(json:string){
-  //   console.log(json)
-  //   // for dev purposes, display the json nicely
-  //   this.displayJsonService.show(json, 'Dedault Data');
-  // }
 
   ngOnDestroy():void {
     this.formPermissionService.formReadOnly = false;
-    // clean up the on submit listener
-    // if(this.submittedSubscription){
-    //   this.submittedSubscription.unsubscribe();
-    // }
+		//
+		// revert this back if you use it
+		// this.formService.disableForm = false;
+
   }
 
 }
